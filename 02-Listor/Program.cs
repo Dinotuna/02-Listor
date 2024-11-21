c5ba78f1d336721bee768b8964859bcaf1ccb6e1
Dino <dino.omicevic@elev.ga.ntig.sem>
2024-11-21 15:23:31 +0100

    first commit

---
+++ 02-Listor/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// string[] toys = {"Rubiks-kub", "Lego", "Play-Doh", "Nalle", "Transformer"};
+
+
+// string[] names = {"Majkel", "Isak", "Albin", "Axel" ,"Sverre"};
+// int[] ints = {1, 3, 6, 7, 9};
+
+
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine($"{names[i]} ger {toys[i]} betyget {ints[i]}");
+// }
+
+
+List<string> cities = new List<string>();
+
+Console.WriteLine("Skriv namn på städer, Skriv exit för att avsluta");
+string svar = "";
+
+while(svar != "exit")
+{
+    svar = Console.ReadLine();
+    cities.Add(svar);
+}
+
+Console.WriteLine("Städerna du skrev var:");
+
+foreach (var city in cities)
+{
+    Console.WriteLine(city);
+}
+
+Console.ReadLine();
